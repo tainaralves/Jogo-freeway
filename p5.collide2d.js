88bd7fb0271c1586a18ada60b836290998b352d3
Tainara Alves <127138538+tainaralves@users.noreply.github.com>
2023-05-08 12:49:46 -0300

    Update p5.collide2d.js

--- p5.collide2d.js
+++ p5.collide2d.js
@@ -1,12 +1,3 @@
-/*
-Repo: https://github.com/bmoren/p5.collide2D/
-Created by http://benmoren.com
-Some functions and code modified version from http://www.jeffreythompson.org/collision-detection
-Version v0.7.3 | June 22, 2020
-CC BY-NC-SA 4.0
-*/
-
-
 p5.prototype._collideDebug = false;
 
 p5.prototype.collideDebug = function(debugMode){
